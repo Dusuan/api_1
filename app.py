fbfb109e7d158bf72d8f55a9539384591d4bbbd0
Dusuan <XxDusuanxX@hotmail.com>
2023-11-07 22:56:12 -0800

    hello

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
-
+from markupsafe import escape
 
 app =  Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "nigga sex"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
